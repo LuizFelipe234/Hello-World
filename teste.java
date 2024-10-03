7d7391f91cd3aeef37f6351d5af8df54806b3914
SY <sykchr@gmail.com>
2024-10-02 21:50:36 -0300

    teste.java

--- teste.java
+++ teste.java
@@ -1,7 +1,7 @@
 package Java1.Java;
 public class teste {
     public static void main(String[] args) {
-        System.out.println("corno");
+        System.out.println("hello World");
 
         
     }
